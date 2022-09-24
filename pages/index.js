5e8a8e0bc9e4d796b409bc595b23c5eac2c84f6e
German Tome <german05tm@gmail.com>
2022-09-24 16:27:57 +0200

    sun + earth + moon
    xd

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,87 @@
 import { useSpring, config, animated } from "@react-spring/three";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
+import { OrbitControls, PointMaterial, Text, Text3D } from "@react-three/drei";
+import {
+	BackSide,
+	MeshPhongMaterial,
+	MeshToonMaterial,
+	Raycaster,
+} from "three";
+
+// softShadows();
 
 export default function Home() {
 	return (
 		<div className="w-screen h-screen">
-			<Canvas>
-				<ambientLight intensity={0.1} />
-				<directionalLight color="red" position={[0, 0, 5]} />
+			<Canvas shadows={true}>
+				<ambientLight intensity={0.2} position={[0, 5, 0]} />
+				<pointLight
+					position={[0, 15, 2]}
+					castShadow={true}
+					shadow-mapSize-height={2048}
+					shadow-mapSize-width={2048}
+				/>
+				<Box size={[2, 2, 2]} color="orange">
+					<Box
+						size={[1, 1, 1]}
+						color="lightblue"
+						offset={5}
+						speed_multiplier={2}
+					>
+						<Box
+							size={[0.4, 0.4, 0.4]}
+							color="gray"
+							offset={1.5}
+							speed_multiplier={4}
+						/>
+					</Box>
+				</Box>
 
-				<Box />
+				<mesh
+					receiveShadow={true}
+					position={[0, -2, 0]}
+					rotation={[Math.PI / 2, 0, 0]}
+				>
+					<planeBufferGeometry args={[50, 50]} />
+					<shadowMaterial
+						attach={"material"}
+						opacity={0.3}
+						side={BackSide}
+					/>
+				</mesh>
+
+				<OrbitControls />
 			</Canvas>
 		</div>
 	);
 }
 
-function Box() {
-	const boxRef = useRef();
-
-	const [active, setActive] = useState(true);
-	const { rotation } = useSpring({
-		rotation: active ? 0 : Math.PI / 2,
-		config: config.wobbly,
-	});
+function Box({ size, color, speed_multiplier = 1, offset = 0, children }) {
+	const [rotation, setRotation] = useState(0);
 
 	useFrame(() => {
-		console.log(boxRef.current.rotation);
+		console.log(rotation);
 
-		boxRef.current.rotation.x += 0.001;
-		boxRef.current.rotation.y += 0.01;
-		boxRef.current.rotation.z += 0.001;
+		setRotation(rotation + 0.003 * speed_multiplier);
 	});
 
 	return (
-		<animated.mesh
-			ref={boxRef}
-			rotation-x={rotation}
+		<mesh
 			rotation-y={rotation}
-			rotation-z={rotation}
-			onClick={() => setActive(!active)}
+			position={[offset, 0, 0]}
+			// onPointerEnter={() => setColor("hotpink")}
+			// onPointerLeave={() => setColor("orange")}
 		>
-			<boxGeometry args={[2, 2, 2]} />
-			<meshStandardMaterial />
-		</animated.mesh>
+			<mesh
+				rotation-x={-rotation}
+				rotation-z={rotation}
+				castShadow={true}
+			>
+				<boxGeometry args={size} rotateX={rotation} />
+				<meshStandardMaterial color={color} rotateX={rotation} />
+			</mesh>
+			{children}
+		</mesh>
 	);
 }
